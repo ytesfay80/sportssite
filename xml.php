d8777455e6c3912524b63ac8c7b040627e355656
ytesfay80 <63468184+ytesfay80@users.noreply.github.com>
2021-03-12 15:03:58 -0500

    Update xml.php

--- xml.php
+++ xml.php
@@ -10,22 +10,9 @@ print_r($xmlFile)
 ?>
 
 
-new shit
 <?php
       foreach ($xml->document as $doc) { ?>
       <h1>Hello:<?php echo $doc->from; ?></h1>
       <?php } ?>
       
-      
-      
-      
-      /* <?xml version="1.0" encoding="UTF-8"?> 
-<documents>
-  <document>
- <title>Forty What?</title>
- <from>Joe</from>
- <to>Jane</to>
- <body>I know that's the answer -- but what's the question?</body>
- </document>
-</documents>
-				*/
+     
